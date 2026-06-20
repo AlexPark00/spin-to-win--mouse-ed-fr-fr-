1a68d1fe974d8dff7c8b39bfde0352c1d8cdb611
Argentumic <106577953+Argentumic@users.noreply.github.com>
2026-06-20 15:40:59 +0300

    main menu, before pathfinding for enemies

---
+++ button_container.gd
@@ -0,0 +1,21 @@
+extends VBoxContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://main.tscn");
+
+func _on_settings_pressed() -> void:
+	$"../../Settings".visible = !$"../../Settings".visible;
+
+func _on_tutorial_pressed() -> void:
+	$"../../Tutorial".visible = !$"../../Tutorial".visible;
